50279c6bb00b71432a3d855d839ecc5adcdb586d
juanjose701 <juanjose.perez@seph.gob.mx>
2024-11-19 20:20:00 -0600

    se suben cambios de proyecto final

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Info date
-const dateNumber = document.getElementById('dateNumber');
+const dateNumber = document.getElementById('dateNumber'); 
 const dateText = document.getElementById('dateText');
 const dateMonth = document.getElementById('dateMonth');
 const dateYear = document.getElementById('dateYear');
